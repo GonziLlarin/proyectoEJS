5cadfad34823da97750ffea10b8fb3c31928dc16
Gonzu <gonziillarin@gmail.com>
2023-09-27 12:30:37 -0300

    add ejs

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 const express = require('express');
+
+const path = require('path');
+
 const mainRoutes = require('./routes/main');
+
 const app = express();
 
 app.use(express.static('public'));
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, './views'));
+
 app.use('/', mainRoutes);
 
 app.listen(3094,()=>{
